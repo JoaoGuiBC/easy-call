59bab369380d35db1c42323ec9921cfdb5fc3e0d
joaoguibc <joaoguibc@gmail.com>
2023-01-01 21:33:44 -0300

    integrate React Hook Form into the application

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,31 @@
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
 import { Button, TextInput } from '@ignite-ui/react'
 
 import { Form } from './styles'
 
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(formData: ClaimUsernameFormData) {
+    console.log(formData.username)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="easycall.com/" placeholder="Seu usuário" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="easycall.com/"
+        placeholder="Seu usuário"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
         Reservar
         <ArrowRight />
